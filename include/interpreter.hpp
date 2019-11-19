17a304b3b5ff4a0ce0592b036780e023ec465556
crollt <crollt@oregonstate.edu>
2019-11-19 12:01:22 -0800

    Merge

--- include/interpreter.hpp
+++ include/interpreter.hpp
@@ -1,7 +1,15 @@
+#ifndef INTERPRETER_HPP
+#define INTERPRETER_HPP
+#include <memory>
 #include "ast.hpp"
 namespace interpreter {
     class Interpreter {
+        std::shared_ptr<ast::Block> ast;
         public:
             Interpreter();
+            Interpreter(ast::Block *);
+
+            void run();
     };
 }
+#endif
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,1 +1,8 @@
-// Placeholder
+#include "interpreter.hpp"
+
+namespace interpreter {
+    Interpreter::Interpreter() {}
+    Interpreter::Interpreter(ast::Block *ast) {
+        this->ast = std::shared_ptr<ast::Block>(ast);
+    }
+}
